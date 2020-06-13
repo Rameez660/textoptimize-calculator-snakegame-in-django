16fb0a00bc68d3213c2e03507e9e46e75f0234c9
Rameez660 <muhammadrameez660@gmail.com>
2020-06-13 09:48:37 +0500

    first committ

---
+++ rameez/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\django\disk c work\django ecommerce website\textutils website\rameez\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
